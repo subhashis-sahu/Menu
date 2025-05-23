ff3e74a3169f856d90820102421054e71b25b0bb
Subhashis Sahu <sahusubhashis01@gmail.com>
2025-05-24 01:10:26 +0530

    test updated

--- app/src/main/java/com/example/menu/Dinner.java
+++ app/src/main/java/com/example/menu/Dinner.java
@@ -8,7 +8,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+public class Dinner extends AppCompatActivity {
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dinner);
